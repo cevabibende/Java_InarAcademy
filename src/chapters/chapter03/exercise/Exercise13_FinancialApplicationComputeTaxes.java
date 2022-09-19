f222f78e82527ffe310ac06db695a69508d9dd8b
Cevabı Bende <mahmutcemrekk@hotmail.com>
2022-09-19 22:31:34 +0300

    19 sept 3

--- src/chapters/chapter03/exercise/Exercise13_FinancialApplicationComputeTaxes.java
+++ src/chapters/chapter03/exercise/Exercise13_FinancialApplicationComputeTaxes.java
@@ -1,5 +1,5 @@
 package chapters.chapter03.exercise;
 
 public class Exercise13_FinancialApplicationComputeTaxes {
-aaaa
+
 }
--- src/chapters/chapter03/exercise/Exercise21_ScienceDayOfWeek.java
+++ src/chapters/chapter03/exercise/Exercise21_ScienceDayOfWeek.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class Exercise21_ScienceDayOfWeek {	
 	
 	public static void main(String[] args) {
-		j
+
 	}
 
 	
